02501e907901a4c93b9fd33a4ca285a1f6e35fef
DaniR1011 <recio.dani10@gmail.com>
2024-12-02 16:59:41 +0100

    final upload

---
+++ 4. EDA con Python/src/limpieza.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+def procesar_dataframe(df, cambiar_nombre_columnas=None, cambiar_tipo_columnas=None):
+    """
+    Función que se encarga de cambiar los nombres de las columnas y sus tipos de datos.
+
+    Args:
+        df (pd.DataFrame): El DataFrame a procesar.
+        nombres_columnas (dict): Modifica el nombre del valor inicial por el segundo valor.
+        cambiar_tipo_columnas (dict): Modifica el tipo de dato del valor inicial por el segundo valor.
+
+    Returns:
+        pd.DataFrame: El DataFrame con los cambios realizados.
+    """
+# Cambia el nombre de las columnas, en caso de recibir el diccionario
+    if cambiar_nombre_columnas:
+        df = df.rename(columns=cambiar_nombre_columnas)
+
+# Cambia el tipo de las columnas, en caso de recibir el diccionario. Para cada tipo de dato realizará
+# una acción, con el fin de corregir valores nulos.
+    if cambiar_tipo_columnas:
+        for columna, tipo in cambiar_tipo_columnas.items():
+            try:
+                if columna in df.columns:
+                    if tipo == "int":
+                        df[columna] = df[columna].fillna(0).astype(int)
+                    elif tipo == "float64":
+                        df[columna] = pd.to_numeric(df[columna].str.replace(',', '.'), errors='coerce')
+                    elif tipo == "datetime64":
+                        df[columna] = pd.to_datetime(df[columna], format='%Y-%m-%d', errors='coerce')
+                    else:
+                        df[columna] = df[columna].astype(tipo)
+                else:
+                    print(f"⚠️ La columna '{columna}' no existe en el DataFrame.")
+            except Exception as error:
+                print(f"❌ Error al convertir la columna '{columna}' a {tipo}: {error}")
+    return df
---
+++ 4. EDA con Python/src/variables.py
@@ -0,0 +1,24 @@
+cambiar_nombre_columnas = {
+ 'emp.var.rate': 'emp_var_rate',
+ 'cons.price.idx': 'cons_price_idx',
+ 'cons.conf.idx': 'cons_conf_idx',
+ 'nr.employed': 'nr_employed',
+ 'NumWebVisitsMonth': 'Num_Web_Visits_Month'
+}
+
+cambiar_tipo_columnas = {
+ "age": "int",
+ "default": "bool",
+ "housing": "bool",
+ "loan": "bool",
+ "cons_price_idx": "float64",
+ "cons_conf_idx": "float64",
+ "euribor3m": "float64",
+ "nr_employed": "float64",
+ "date": "datetime64",
+ "id_": "string",
+ "Kidhome": "int",
+ "Teenhome": "int",
+ "Dt_Customer": "datetime64",
+ "Num_Web_Visits_Month": "int"
+}
